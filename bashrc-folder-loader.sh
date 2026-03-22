84d447613f30825d86ddb98c3f977879f922714b
Oliver Kaup <git@okaup.de>
2026-03-22 20:24:30 +0100

    bashrc folder loader with bashrc and sh files, snippet will be updated if present

--- bashrc-folder-loader.sh
+++ bashrc-folder-loader.sh
@@ -9,7 +9,7 @@ SNIPPET=$(cat <<'EOF'
 # >>> bashrc.d loader >>>
 # Source additional shell configurations
 if [ -d "$HOME/.bashrc.d" ]; then
-    for config in "$HOME/.bashrc.d"/*.sh; do
+    for config in "$HOME/.bashrc.d"/*.{bashrc,sh}; do
         [ -r "$config" ] && source "$config"
     done
 fi
@@ -22,9 +22,12 @@ touch "$BASHRC"
 
 # Check if marker already exists
 if grep -qF "$START_MARK" "$BASHRC"; then
-    echo "bashrc.d loader already present. Skipping."
+    echo "bashrc.d loader already present. Updating."
+    # Extract the content before the start mark and after the end mark
+    before=$(sed -n "1,/$(echo "$START_MARK" | sed 's/[[/.*^$]/\\&/g')/p" "$BASHRC" | sed '/'"$(echo "$START_MARK" | sed 's/[[/.*^$]/\\&/g')"'/d')
+    after=$(sed -n '/'"$(echo "$END_MARK" | sed 's/[[/.*^$]/\\&/g')"'/,$ p' "$BASHRC" | tail -n +2)
+    printf "%s\n\n%s\n%s\n" "$before" "$SNIPPET" "$after" > "$BASHRC"
 else
     echo "Adding bashrc.d loader to $BASHRC"
     printf "\n%s\n" "$SNIPPET" >> "$BASHRC"
 fi
-
