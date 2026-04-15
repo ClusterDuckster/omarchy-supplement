cfbe184f0ece0cef6a23547cd4d33dc10e4b9b98
Oliver Kaup <git@okaup.de>
2026-04-15 07:00:36 +0200

    zen-browser use precompiled AUR version

--- install-zen-browser.sh
+++ install-zen-browser.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # install zen-browser from AUR
-yay -S --noconfirm zen-browser
+yay -S --noconfirm zen-browser-bin
 
 # TODO: what are the zen-browser-xxx extension AUR packages about?
 
